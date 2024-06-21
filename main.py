2b1b22fea096a88c61918cbacbcf38cf493db7cb
Gekk0 <gekk0dw@gmail.com>
2024-06-21 13:13:28 +0300

    mounted samba, total execution time categories

--- main.py
+++ main.py
@@ -17,12 +17,25 @@ async def root():
 
 @app.get("/enhance_folder/{folder_path}")
 async def enhance_folder(folder_path: str):
-    # folder_path = os.path.join('mounted disk', folder_path)
-    folder_path = os.path.join(r'C:\Users\Gekk0\Pictures\Camera Roll', folder_path.replace('_', "/"))
+    folder_path = os.path.join(r'Y:/test enhancement',  folder_path.replace('_', "/"))
+    # folder_path = os.path.join(r'C:\Users\Gekk0\Pictures\Camera Roll', folder_path.replace('_', "/"))
     print(folder_path)
 
-    total_time = await process_folder(folder_path)
+    if not os.path.isdir(folder_path):
+        return {"message": f"Folder {folder_path} does not exist"}
 
-    print(f"Total execution time: {total_time:.2f} seconds")
+    try:
+        total_time, open_save_time, enhance_time = await process_folder(folder_path)
+        print(f"Total execution time: {total_time:.2f} seconds")
+        print(f"Open-save time: {open_save_time:.2f} seconds")
+        print(f"Enhance time: {enhance_time:.2f} seconds")
+        return {"message": f"Folder {folder_path} processed successfully"}
+    except Exception as e:
+        return {f"An error occurred: {e}"}
 
-    return {"message": f"Folder {folder_path} processed successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
--- photoshop_enhancer.py
+++ photoshop_enhancer.py
@@ -6,12 +6,15 @@ from photoshop.api import JPEGSaveOptions
 
 
 async def process_folder(folder: str):
+    open_save_time = 0
+    enhance_time = 0
+
     action = identify_action(folder)
 
     if not action:
         return
 
-    image_files = [f for f in os.listdir(folder) if f.endswith('.jpg')]
+    image_files = [f for f in os.listdir(folder) if f.lower().endswith('.jpg')]
     start_time = time.time()
 
     try:
@@ -19,19 +22,33 @@ async def process_folder(folder: str):
             ps.app.displayDialogs = DialogModes.DisplayNoDialogs
 
             for image_name in image_files:
+                print(f"Processing image: {image_name}")
                 image_path = os.path.join(folder, image_name)
+
+                open_save_start_time = time.time()
                 doc = ps.app.open(image_path)
-                ps.app.doAction(action, "Default Actions")
                 options = JPEGSaveOptions(quality=5)
                 doc.saveAs(image_path, options, asCopy=False)
                 doc.close()
+                open_save_end_time = time.time()
+
+                open_save_time += open_save_end_time - open_save_start_time
+
+                enhance_start_time = time.time()
+                doc = ps.app.open(image_path)
+                ps.app.doAction(action, "Default Actions")
+                doc.saveAs(image_path, options, asCopy=False)
+                doc.close()
+                enhance_end_time = time.time()
+
+                enhance_time += enhance_end_time - enhance_start_time
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
     end_time = time.time()
     total_time = end_time - start_time
-    return total_time
+    return total_time, open_save_time, enhance_time
 
 
 def identify_action(folder):
@@ -39,7 +56,7 @@ def identify_action(folder):
         'Силуэт': 'silhouette_action',
         'Отражение': 'reflect_action',
         'Портрет': 'portrait_action',
-        'KZ': 'kz_action'
+        'KZ': 'kz_action',
     }
 
     for studio_action in studio_actions.keys():
